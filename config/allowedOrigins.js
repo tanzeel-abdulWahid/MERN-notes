a19d9321d16b742016873442d7e9577266f384be
Tanzeel <65041070+tanzeel-abdulWahid@users.noreply.github.com>
2022-11-27 20:03:50 +0500

    origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
-        'http://localhost:3000',
-        'http://127.0.0.1:5173'
+        // 'http://localhost:3000',
+        // 'http://127.0.0.1:5173',
+        'https://mern-notes-production.up.railway.app/'
 ]
 
 export default allowedOrigins;
